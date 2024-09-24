51deda49af045fe8326fcabaefce75d0c2d92edc
Caio David Urano de Carvalho. <161221118+CaioDavid01@users.noreply.github.com>
2024-09-24 06:41:53 -0300

    Add files via upload
    JogodaVelhaYacal

---
+++ JogodaVelha/src/jogodavelha/JogodaVelha.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jogodavelha;
+
+
+/**
+ *
+ * @author Caio David
+ */
+public class JogodaVelha extends javax.swing.Jframe{
+    private String começarJogo = "X";
+    private int Jogador1 = 0;
+    private int Jogador2 = 0;
+    boolean checker;
+    
+    public class jogodaVelha(){
+        initComponents();
+    }
+    
+    private void gamescore()
+    {  
+     Jogador1.setText(String.valueOf(Jogador1count));
+     Jogador2.setText(String.valueOf(Jogador2count));
+    }
+    
+     private void choose_a_player()
+    {  
+     if (startGame.e)
+    }
+     
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
